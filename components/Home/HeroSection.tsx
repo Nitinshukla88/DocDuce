64afea2952cec4689489bb63e3a6fb4c23100a8d
nitinshukla <nitin.shukla12004@gmail.com>
2025-05-02 17:42:09 +0530

    added navbar into the code

---
+++ components/Home/HeroSection.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+export default function HeroSection() {
+  return (
+    <section>
+      <div className="">
+        <p>Powered by AI</p>
+        <h1>Transform PDFs into concise summaries</h1>
+        <h2>Get a beautiful summary reel of a document in seconds</h2>
+        <Button>Try DocDuce</Button>
+      </div>
+    </section>
+  );
+}
