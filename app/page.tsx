bb78445c38a87ef80d965c89bb0600869af9a973
nitinshukla <nitin.shukla12004@gmail.com>
2025-05-04 02:52:52 +0530

    added the PricingSection into the website

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import BgGradient from "@/components/common/BgGradient";
 import DemoSection from "@/components/Home/DemoSection";
 import HeroSection from "@/components/Home/HeroSection";
 import HowItWorksSection from "@/components/Home/HowItWorksSection";
+import PricingSection from "@/components/Home/PricingSection";
 
 export default function Home() {
   return (
@@ -11,10 +12,10 @@ export default function Home() {
           <HeroSection />
           <DemoSection />
           <HowItWorksSection />
+          <PricingSection />
         </div>
       </BgGradient>
 
-      {/* <PricingSection /> */}
       {/* <CTASection /> */}
     </div>
   );
